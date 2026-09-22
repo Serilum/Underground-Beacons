5171aefe6f4fd795c8bb44698c5eec6af7e8fc7e
Rick South <rick@natamus.com>
2026-09-22 15:57:52 +0200

    Updated Underground Beacons to Minecraft version 26.3.0.

--- Common/src/main/java/com/natamus/undergroundbeacons/util/Reference.java
+++ Common/src/main/java/com/natamus/undergroundbeacons/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "undergroundbeacons";
 	public static final String NAME = "Underground Beacons";
 	public static final String VERSION = "1.1";
-	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
+	public static final String ACCEPTED_VERSIONS = "[26.3.0]";
 }
